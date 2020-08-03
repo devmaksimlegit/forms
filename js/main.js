859f97a28707401501891001a94dd759c1f6be07
devmaksimlegit <68745507+devmaksimlegit@users.noreply.github.com>
2020-08-03 03:42:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-const input = document.querySelector('.form__input');
-const form = document.querySelector('.form');
+const forms = document.querySelectorAll('.form');
 
-form.onsubmit = function () {
-  const arr = input.value.split('');
-  let outputArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] - 0 === NaN) {
-      outputArr[i] = arr[i];
+forms.forEach(function (form) {
+  form.onsubmit = function (event) {
+    event.preventDefault();
+    const inputs = this.querySelectorAll('.form__input--password');
+
+    for (let input of inputs) {
+      const letters = input.value.split('');
+      let numsArr = [];
+      let strsArr = [];
+      for (let letter of letters) {
+        if (isNaN(letter - 0)) {
+          strsArr.push(letter);
+        } else {
+          numsArr.push(letter);
+        }
+      }
+      if (numsArr.length < 5 || strsArr.length < 2) {
+        input.classList.add('form__input--error');
+      } else {
+        input.classList.remove('form__input--error');
+      }
+    }
+    let errorsArr = [];
+    for (let input of inputs) {
+      if (input.classList.contains('form__input--error')) {
+        errorsArr.push(input);
+      }
+    }
+    if (errorsArr.length > 0) {
+      this.classList.add('form--error');
     } else {
-      outputArr[i] = arr[i] - 0;
+      this.classList.remove('form--error');
+      this.submit();
     }
   }
-  console.log(outputArr);
-}
+});
